cdbdbb89be3063c62a32767fea98043750a4a6f1
TTG\darekar <sawan.darekar@tomtom.com>
2020-11-16 20:48:39 +0530

    ConcurrencyWithExecutors added

--- docker-spring-boot-war/src/main/java/com/dockerTest/springboot/dockerspringbootwar/DockerSpringBootWarApplication.java
+++ docker-spring-boot-war/src/main/java/com/dockerTest/springboot/dockerspringbootwar/DockerSpringBootWarApplication.java
@@ -3,7 +3,6 @@ package com.dockerTest.springboot.dockerspringbootwar;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-// import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class DockerSpringBootWarApplication extends SpringBootServletInitializer {
